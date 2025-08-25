faac0a703d47b2a06083dda83c345319e3d520cc
Muhammed Aslam KH <akh041781@gmail.com>
2025-08-25 22:29:05 +0530

    calculator

--- calculator.php
+++ calculator.php
@@ -1,5 +1,7 @@
 <html>
 <body>
 <center>
-<tr><th><input type="text" name=""
+<tr><th>FIRST NUMBER</th><th><input type="text" name="first"></th></tr>
+<tr><th>SECOND NUMBER</th><th><input type="text" name="second"></th></tr>
+
 
